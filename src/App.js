1f65431cbd1f2332a047da0f9d4b75a3dd15b90a
jseg787 <jsegarra3@gmail.com>
2020-12-23 20:31:41 -0500

    add connectin to weather api and data to main component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	state = {
+		weather: {},
+		name: '',
+		main: {}
+	};
+
+	componentDidMount = async () => {
+		await this.getWeather();
+	};
+
+	getWeather = async () => {
+		const city = 'brooklyn';
+		const key = process.env.REACT_APP_WEATHER_API_KEY;
+		const response = await fetch(
+			`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=imperial`
+		);
+		const data = await response.json();
+		const { main, name, weather } = data;
+		this.setState({ main, name, weather: weather[0] });
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<div>
+					<img
+						src={`http://openweathermap.org/img/wn/${this.state.weather.icon}@2x.png`}
+						alt={this.state.weather.main}
+					/>
+				</div>
+				<div>{this.state.name}</div>
+				<div>{this.state.main.temp}&deg; F</div>
+			</div>
+		);
+	}
 }
 
 export default App;
